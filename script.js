6820ed1772d446fdc7b3e110168e2ef9adbce155
Karl Bohn <karl@McBain>
2023-10-23 16:45:52 -0400

    font adjustment, opens Google search for fruit when form is submitted

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
+const form = document.querySelector('form');
 
 const fruits = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 const lowerCaseFruit = [];
@@ -27,8 +28,19 @@ function showSuggestions(results, inputVal) {
 }
 
 function useSuggestion(e) {
-	// set clicked value to input.value
+	if(e.target.tagName === 'LI'){
+		let li = e.target;
+		input.value = li.innerText;
+		suggestions.innerHTML = '';
+	}
+}
+
+function doSearch(){
+	let searchInput = input.value;
+	let url = "https://www.google.com/search?q=" + searchInput;
+	window.open(url);
 }
 
 input.addEventListener('input', searchHandler);
+form.addEventListener('submit', doSearch);
 suggestions.addEventListener('click', useSuggestion);
